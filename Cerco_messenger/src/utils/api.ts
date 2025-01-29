7318887718488ec8cb143705c1ed8608da159736
InoDash <81249652+inocent345f@users.noreply.github.com>
2025-01-29 20:38:42 +0100

    Update api.ts

--- Cerco_messenger/src/utils/api.ts
+++ Cerco_messenger/src/utils/api.ts
@@ -11,12 +11,12 @@ interface User {
   email?: string;
 }
 
-interface Message {
+interface message {
   id: string;
-  senderId: string;
-  receiverId: string;
-  text: string;
-  timestamp: number;
+  created_at?: string;
+  chat_id?: string;
+  message?: string;
+  username?: string;
 }
 
 const headers = {
@@ -60,33 +60,6 @@ export const getUsers = async () => {
   }
 };
 
-// Fonction pour envoyer un message
-export const sendMessage = async (message: { receiverId: string; text: string }) => {
-  try {
-    const response = await axios.post(`${API_URL}/messages`, message, {
-      headers,
-      withCredentials: true, // Assurez-vous que le token de session est envoyé
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Erreur dans l\'envoi du message:', error);
-    throw new Error(error.response?.data?.detail || 'Une erreur est survenue lors de l\'envoi du message');
-  }
-};
-
-// Fonction pour récupérer les messages
-export const getMessages = async (receiverId: string) => {
-  try {
-    const response = await axios.get(`${API_URL}/messages/${receiverId}`, {
-      headers,
-      withCredentials: true, // Assurez-vous que le token de session est envoyé
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Erreur dans la récupération des messages:', error);
-    throw new Error(error.response?.data?.detail || 'Une erreur est survenue lors de la récupération des messages');
-  }
-};
 
 // Fonction pour vérifier l'OTP (One Time Password)
 export const verifyOtp = async (data: { email: string; token: string; type: string }) => {
@@ -102,3 +75,71 @@ export const verifyOtp = async (data: { email: string; token: string; type: stri
     }
   }
 };
+
+
+// Fonction pour récupérer l'ID d'un utilisateur à partir de son username
+export const getUserIdByUsername = async (username: string) => {
+  const response = await axios.get(`${API_URL}/users?username=${username}`);
+  if (response.data && response.data.length > 0) {
+    return response.data[0].id; // Supposons que l'ID soit dans le premier élément
+  }
+  throw new Error("Utilisateur non trouvé");
+};
+
+
+//-------------------------------------------- Messages ---------------------------------------------------------------//
+
+// Fonction pour envoyer un message
+export const sendMessage = async (message: { receiverId: string; text: string }) => {
+  try {
+    const response = await axios.post(`${API_URL}/messages`, message, {
+      headers,
+      withCredentials: true, // Assurez-vous que le token de session est envoyé
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Erreur dans l\'envoi du message:', error);
+    throw new Error(error.response?.data?.detail || 'Une erreur est survenue lors de l\'envoi du message');
+  }
+};
+
+
+// Fonction pour récupérer le chat_id de deux utilisateurs
+export const getChatId = async (user1_id:string, user2_id: string) => {
+  console.log(user1_id)
+  try {
+    const response = await axios.get(`${API_URL}/get-chat-id?user1_id=${user1_id}&user2_id=${user2_id}`);
+    return response.data; // Retourner le chat_id
+    console.log(response.data)
+  } catch (error) {
+    console.error('Erreur lors de la récupération du chat_id:', error);
+    throw new Error(error.response?.data?.detail || 'Erreur lors de la récupération du chat_id');
+  }
+};
+
+
+// Fonction pour récupérer les messages
+export const getMessages = async (chatId: string) => {
+  try {
+    const response = await axios.post(`${API_URL}/messages/?chat_id=${chatId}`);
+    console.log(chatId)
+    return response.data; // Retourner les messages récupérés
+  } catch (error) {
+    console.error('Erreur lors de la récupération des messages:', error);
+    throw new Error(error.response?.data?.detail || 'Erreur lors de la récupération des messages');
+  }
+};
+
+// Fonction pour récupérer les détails de l'utilisateur par ID
+export const getUserByUsername = async (username: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/user?username=${username}`);
+    return response.data; // Retourner les détails de l'utilisateur
+    console.log(response)
+  } catch (error) {
+    console.error('Erreur lors de la récupération des détails de l\'utilisateur:', error);
+    throw new Error(error.response?.data?.detail || 'Erreur lors de la récupération des détails de l\'utilisateur');
+  }
+};
+
+//-------------------------------------------------------------------------------------------------------------
